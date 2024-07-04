037f0f2c3ead448fd5161dff6bb1235e56836092
kevin <admin@w3c.top>
2024-07-04 22:35:45 +0800

    diff: 乱序下的diff比对

--- packages/test/reactivity.js
+++ packages/test/reactivity.js
@@ -4,17 +4,22 @@ import { h } from '../runtime-core/src/h.js'
 import { render } from "../runtime-dom/src/index.js"
 
 const { effect, reactive, ref, computed, Text, Comment, Fragment } = Vue
-
 const vnode = h('ul', [
-  // h('li', {
-  //   key: 1
-  // }, 'a'),
+  h('li', {
+    key: 1
+  }, 'a'),
   h('li', {
     key: 2
   }, 'b'),
   h('li', {
     key: 3
   }, 'c'),
+  h('li', {
+    key: 4
+  }, 'd'),
+  h('li', {
+    key: 5
+  }, 'e')
 ])
 // 挂载
 render(vnode, document.querySelector('#app'))
@@ -23,14 +28,20 @@ render(vnode, document.querySelector('#app'))
 setTimeout(() => {
   const vnode2 = h('ul', [
     h('li', {
-      key: 4
-    }, 'c'),
-    h('li', {
-      key: 2
-    }, 'b'),
+      key: 1
+    }, 'new-a'),
     h('li', {
       key: 3
-    }, 'd')
+    }, 'new-c'),
+    h('li', {
+      key: 2
+    }, 'new-b'),
+    h('li', {
+      key: 6
+    }, 'new-f'),
+    h('li', {
+      key: 5
+    }, 'new-e'),
   ])
   render(vnode2, document.querySelector('#app'))
 }, 2000);
